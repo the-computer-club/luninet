891a418ac0b0db5f2b51adcc4cd94cd53e267dce
Skarlett <Skarlett@users.noreply.github.com>
2026-02-15 15:15:00 -0600

    update

--- flake.nix
+++ flake.nix
@@ -84,7 +84,7 @@
 
         update-inventory = pkgs.writeShellScriptBin "update-inventory"
           ''
-          ${pkgs.lib.getExe ip-allocate} luni ${
+          ${pkgs.lib.getExe ip-allocate} $@ luni ${
             pkgs.writeText "current-inventory.json"
               (builtins.toJSON luninet)
           }
