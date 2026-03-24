1c6984727df827d461a9e4ff2cb193410c2a1b8b
Skarlett <Skarlett@users.noreply.github.com>
2026-03-24 11:39:08 -0500

    trim: peers.nix

--- peers.nix
+++ peers.nix
@@ -1,26 +1,10 @@
 {
-
-  # cardinal = {
-  #   isController = true;
-  #   publicKey = "GfzR2IYKx6IBXwGVNSTVnCRcQSr/Wm5dlPwXHO2xykI=";
-  #   selfEndpoint = "72.11.152.185:31690";
-  # };
-
   gateway = {
     isController = true;
     publicKey = "GfzR2IYKx6IBXwGVNSTVnCRcQSr/Wm5dlPwXHO2xykI=";
     selfEndpoint = "198.12.96.43:32288";
     ipv4 = ["172.29.80.0/24"];
     ipv6 = ["fd49:093b:2b68::/48"];
-
-    # TODO: Add IPv4 port mapping.
-    # portmap = [
-    #   { # create a port on (dst-port) -> ${to-address}:${to-port}
-    #     dst-port = 25543;
-    #     to-port = 25543;
-    #     to-address = "10.0.0.1"; # or peers.${hostname}.ipv4
-    #   }
-    # ];
   };
 
   simcra.publicKey = "pq529jYwkJZZzdWB2fJ08A+41prV5gVsg3iE/kVN0GQ=";
